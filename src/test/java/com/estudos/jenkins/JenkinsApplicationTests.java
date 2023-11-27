cdceb5ea512a9e9b3431eee4109680010a6e6439
cesar.sena@maps.com.br <cesar.sena@maps.com.br>
2023-11-27 14:48:25 -0300

    Initial commit

---
+++ src/test/java/com/estudos/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.estudos.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(1,1);
+	}
+}
